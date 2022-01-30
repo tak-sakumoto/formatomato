eea5120b53e70a2796d7dcfca4dc1de8e638f2f6
Takuya SAKUMOTO <tak.sakumoto@gmail.com>
2022-01-31 04:43:16 +0900

    Add CSV columns for widths and heights of bboxes

--- for_coco/make_df.py
+++ for_coco/make_df.py
@@ -15,29 +15,24 @@ def make_df(dataset, images, class_df, normalize=False):
     class_ids = [ann['category_id'] for ann in anns]
     classes = [class_df.loc[class_id, 'CLASS'] for class_id in class_ids]
     if normalize:
-        xmins = [
-            ann['bbox'][0] / images.loc[ann['image_id'], 'WIDTH']
-            for ann in anns
-        ]
-        ymins = [
-            ann['bbox'][1] / images.loc[ann['image_id'], 'HEIGHT']
-            for ann in anns
-        ]
-        xmaxs = [
-            (ann['bbox'][0] + ann['bbox'][2])
-            / images.loc[ann['image_id'], 'WIDTH']
-            for ann in anns
-        ]
-        ymaxs = [
-            (ann['bbox'][1] + ann['bbox'][3])
-            / images.loc[ann['image_id'], 'HEIGHT']
-            for ann in anns
-        ]
+        xmins, ymins, xmaxs, ymaxs, w, h = list(zip(*[[
+                ann['bbox'][0] / images.loc[ann['image_id'], 'WIDTH'],
+                ann['bbox'][1] / images.loc[ann['image_id'], 'HEIGHT'],
+                (ann['bbox'][0] + ann['bbox'][2]) / images.loc[ann['image_id'], 'WIDTH'],
+                (ann['bbox'][1] + ann['bbox'][3]) / images.loc[ann['image_id'], 'HEIGHT'],
+                (ann['bbox'][2]) / images.loc[ann['image_id'], 'WIDTH'],
+                (ann['bbox'][3]) / images.loc[ann['image_id'], 'HEIGHT']
+            ] for ann in anns]))
+
     else:
-        xmins = [ann['bbox'][0] for ann in anns]
-        ymins = [ann['bbox'][1] for ann in anns]
-        xmaxs = [ann['bbox'][0] + ann['bbox'][2] for ann in anns]
-        ymaxs = [ann['bbox'][1] + ann['bbox'][3] for ann in anns]
+        xmins, ymins, xmaxs, ymaxs, w, h = list(zip(*[[
+                ann['bbox'][0],
+                ann['bbox'][1],
+                ann['bbox'][0] + ann['bbox'][2],
+                ann['bbox'][1] + ann['bbox'][3],
+                ann['bbox'][2],
+                ann['bbox'][3]
+            ] for ann in anns]))
 
     df_dict = {
         'IMAGE_ID': img_ids,
@@ -46,7 +41,9 @@ def make_df(dataset, images, class_df, normalize=False):
         'X_MIN': xmins,
         'Y_MIN': ymins,
         'X_MAX': xmaxs,
-        'Y_NAX': ymaxs
+        'Y_MAX': ymaxs,
+        'WIDTH': w,
+        'HEIGHT': h
     }
 
     # Making a Pandas Dataframe
